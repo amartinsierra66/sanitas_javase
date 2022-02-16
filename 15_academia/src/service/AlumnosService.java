16069173a8e826bf7d0f24cb8ce814dd31860cda
Antonio Martín <ajms66@gmail.com>
2022-02-16 15:24:09 +0100

    ejercicios1_15_completado 15

--- 15_academia/src/service/AlumnosService.java
+++ 15_academia/src/service/AlumnosService.java
@@ -1,24 +1,35 @@
 package service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import model.Alumno;
 
 public class AlumnosService {
 	private ArrayList<Alumno> alumnos=new ArrayList<>();
 	//recoge alumno y lo guarda
-	public void agregarAlumno(Alumno alumno) {
-		
-	}
-	public void agregarAlumno(String nombre, String curso, double nota) {
-		
-	}
-	//devuelve la nota media global
-	public double media() {
-		
-	}
-	//devuelve conjunto con los alumnos del curso
-	public HashSet<Alumno> alumnosCurso(String curso){
-		
-	}
+	   public void agregarAlumno (Alumno alumno) {
+		   alumnos.add(alumno);
+	    }
+	    public void agregarAlumno (String nombre, String curso, double nota) {
+	        Alumno alumno = new Alumno(nombre, curso, nota);
+	        agregarAlumno(alumno);
+	    }
+	    //devuelve la nota media global
+	    public double media() {
+	        double media = 0;
+	        for (Alumno alumno:alumnos) {
+	            media+=alumno.getNota();
+	        }
+	        return media/alumnos.size();
+	    }
+	    public HashSet<Alumno> alumnosCurso(String curso){
+	        HashSet<Alumno> alumnosC = new HashSet<Alumno>();
+	        for(Alumno alumno:alumnos) {
+	            if(alumno.getCurso().equals(curso)) {
+	                alumnosC.add(alumno);
+	            }
+	        }
+	        return alumnosC;
+	    }
 }
